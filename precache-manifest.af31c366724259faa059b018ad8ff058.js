154d73fea78bf059d8f3c3b34528ad751d1a44d4
Roman Konovalov <konovalov.roman.34@gmail.com>
2020-01-29 01:54:00 +0400

    Updates

---
+++ precache-manifest.af31c366724259faa059b018ad8ff058.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b2beae91b651f7ef4b8869a5a91901",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f5ddf619deba535d954",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "61351c8fc4b87107c7f5",
+    "url": "./static/css/main.306c4f74.chunk.css"
+  },
+  {
+    "revision": "0f5ddf619deba535d954",
+    "url": "./static/js/2.ad7eb4a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ad7eb4a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61351c8fc4b87107c7f5",
+    "url": "./static/js/main.a7cfb275.chunk.js"
+  },
+  {
+    "revision": "2266c1072cb5caf589aa",
+    "url": "./static/js/runtime-main.679e2330.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
